0f0c194983771e834a0d7f7fee1dac89337f1eb3
nickgreenway <nickgreenway@macbook-pro.local>
2015-10-09 09:49:09 -0400

    Initial Commit

---
+++ Seconds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Seconds
+//
+//  Created by Nick Greenway on 10/9/15.
+//  Copyright © 2015 Nick Greenway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
